f621fed0ced786e52e2b10bdb3cd626c65d358de
Philipp Arndt <2f.mail@gmx.de>
2016-01-06 13:42:24 +0100

    ability to select the AI

--- de.rnd7.kata.reversi/src/main/java/de/rnd7/kata/reversi/ui/GameDialog.java
+++ de.rnd7.kata.reversi/src/main/java/de/rnd7/kata/reversi/ui/GameDialog.java
@@ -2,6 +2,7 @@ package de.rnd7.kata.reversi.ui;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
@@ -15,37 +16,36 @@ import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Canvas;
-import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.MessageBox;
 import org.eclipse.swt.widgets.Shell;
 
+import com.google.common.collect.ImmutableList;
+
 import de.rnd7.kata.reversi.logic.GameLogic;
 import de.rnd7.kata.reversi.logic.ai.AIMatrix;
 import de.rnd7.kata.reversi.logic.ai.AlphaBetaPruningAI;
+import de.rnd7.kata.reversi.logic.ai.MatrixAI;
+import de.rnd7.kata.reversi.logic.ai.MatrixAI2;
+import de.rnd7.kata.reversi.logic.ai.MinimaxAI;
 import de.rnd7.kata.reversi.logic.ai.ReversiAI;
 import de.rnd7.kata.reversi.model.Cell;
 import de.rnd7.kata.reversi.model.CellState;
 import de.rnd7.kata.reversi.model.Coordinate;
 
 public class GameDialog {
-	private final ReversiAI white;
-	private final ReversiAI black;
-	// private CellState player = CellState.WHITE;
-	// private GameField field;
+	private ReversiAI white;
 	private final Shell shell;
+
+	private final AIMatrix matrix = AIMatrix.fromResource("matrix.txt");
+	private final ImmutableList<ReversiAI> ais = ImmutableList.of(new MatrixAI(this.matrix), new MatrixAI2(new MatrixAI(this.matrix)), new AlphaBetaPruningAI(), new MinimaxAI());
+	private final Map<String, ReversiAI> aiMap = this.ais.stream().collect(Collectors.toMap(x -> x.getClass().getSimpleName(), x -> x));
 
 	private final GameController controller = new GameController();
 
 	public GameDialog() throws IOException {
-		final AIMatrix matrix = AIMatrix.fromResource("matrix.txt");
-
-		this.white = new AlphaBetaPruningAI(); // MatrixAI(matrix);
-		this.black = null; // new MatrixAI2(new
-							// MatrixAI(matrix));
-
-		// this.field = newField();
 
 		final Display display = new Display();
 		this.shell = new Shell(display);
@@ -53,11 +53,11 @@ public class GameDialog {
 		this.shell.setLayout(new GridLayout());
 		this.shell.setLayoutData(new GridData(GridData.FILL_BOTH));
 
-		final Composite composite = new Composite(this.shell, SWT.NONE);
-		composite.setLayout(new GridLayout(4, false));
-		composite.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+		final Combo combo = new Combo(this.shell, SWT.READ_ONLY);
+		combo.setItems(this.aiMap.keySet().stream().toArray(i -> new String[i]));
+		this.registerChangeListener(combo);
 
-		final Button aiPlay = new Button(composite, SWT.NONE);
+		final Button aiPlay = new Button(this.shell, SWT.NONE);
 		aiPlay.setText("AI Play");
 
 		final Label label = new Label(this.shell, SWT.NONE);
@@ -94,6 +94,19 @@ public class GameDialog {
 		display.dispose();
 	}
 
+	private void registerChangeListener(final Combo combo) {
+		final SelectionAdapter selectionAdapter = new SelectionAdapter() {
+			@Override
+			public void widgetSelected(final SelectionEvent e) {
+				GameDialog.this.white = GameDialog.this.aiMap.get(combo.getText());
+			}
+		};
+
+		combo.addSelectionListener(selectionAdapter);
+		combo.select(0);
+		selectionAdapter.widgetSelected(null);
+	}
+
 	private Optional<Cell> toCell(final MouseEvent e) {
 		final int x = e.x / GameFieldRenderer.CELL_SIZE;
 		final int y = e.y / GameFieldRenderer.CELL_SIZE;
@@ -109,7 +122,13 @@ public class GameDialog {
 		if (!possibleCells.isEmpty()) {
 			final List<Coordinate> possibleMoves = possibleCells.stream().map(Cell::getCoordinate).collect(Collectors.toList());
 			final Coordinate move = this.white.getMove(this.controller.getField(), CellState.WHITE, possibleMoves);
-			this.controller.doMove(CellState.WHITE, move);
+			try {
+				this.controller.doMove(CellState.WHITE, move);
+			} catch (final Exception e1) {
+				final MessageBox messageBox = new MessageBox(this.shell, SWT.OK | SWT.ICON_INFORMATION);
+				messageBox.setMessage(e1.getMessage());
+				messageBox.open();
+			}
 		}
 
 		canvas.redraw();
